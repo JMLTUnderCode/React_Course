059c30001a20812ba2cf392eab6f762ad9a58fdf
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-08-21 14:10:27 -0400

    Inclusion de proyecto 05 sobre React Router. Esta primera etapa se estudia desde las bases que es una MPA y como pasar a una SPA sin conceptos y componentes de React, sino desde la manera mas basica y simple posible.

---
+++ projects/05-ReactRouter/src/App.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const NAVIGATION_STATE = 'pushstate';
+
+function Navigate(href : string | URL | null) {
+  window.history.pushState({}, '', href);
+  const navigationEvent = new Event(NAVIGATION_STATE);
+  window.dispatchEvent(navigationEvent);
+}
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home Page!</p>
+      <button onClick={() => Navigate('/about')}>Go to About Page</button>
+    </div>
+  )
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <div>
+        <img src="https://avatars.githubusercontent.com/u/45317638?v=4" alt="Picture of Junior Lara" />
+      </div>
+      <p>Welcome to the About Page!</p>
+      <button onClick={() => Navigate('/')}>Go to Home Page</button>
+    </div>
+  )
+}
+
+function App() {
+  const [currentPath, setCurrentPath] = React.useState(window.location.pathname);
+  
+  React.useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    }
+
+    window.addEventListener(NAVIGATION_STATE, onLocationChange);
+
+    return () => {
+      window.removeEventListener(NAVIGATION_STATE, onLocationChange);
+    }
+  });
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
+  )
+}
+
+export default App
