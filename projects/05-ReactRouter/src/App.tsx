77be9580d5a95e1221c3a5b0a7594f291abbbf85
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-08-22 17:43:50 -0400

    Se implemente Lazy Loading para optimizar los componentes que se deben cargar. Adicionalmente se tiene soporte de aboutpage en cuanto a dos idiomas es/en para simular internacionalidad

--- projects/05-ReactRouter/src/App.tsx
+++ projects/05-ReactRouter/src/App.tsx
@@ -1,12 +1,30 @@
-import { HomePage } from "./pages/Home";
-import { AboutPage } from "./pages/About";
-import { SearchPage } from "./pages/Search";
-import { Page404 } from "./pages/Page404";
+import React from "react";
 import { Router } from "./components/Router";
 import { Route } from "./components/Route";
 import type { RouteProps } from "./types";
 
+const HomePage = React.lazy(() => {
+  return import("./pages/Home")
+    .then(module => ({ default: module.HomePage }));
+});
+const AboutPage = React.lazy(() => {
+  return import("./pages/About")
+    .then(module => ({ default: module.AboutPage }));
+});
+const SearchPage = React.lazy(() => {
+  return import("./pages/Search")
+    .then(module => ({ default: module.SearchPage }));
+});
+const Page404 = React.lazy(() => {
+  return import("./pages/Page404")
+    .then(module => ({ default: module.Page404 }));
+});
+
 const appRoutes: RouteProps[] = [
+  {
+    path: '/:lang/about',
+    Component: AboutPage
+  },
   {
     path: '/search/:query',
     Component: SearchPage
@@ -16,10 +34,12 @@ const appRoutes: RouteProps[] = [
 function App() {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404} >
-        <Route path="/" Component={HomePage} />
-        <Route path="/about" Component={AboutPage} />
-      </Router>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Router routes={appRoutes} defaultComponent={Page404} >
+          <Route path="/" Component={HomePage} />
+          <Route path="/about" Component={AboutPage} />
+        </Router>
+      </React.Suspense>
     </main>
   )
 }
--- projects/05-ReactRouter/src/pages/About.tsx
+++ projects/05-ReactRouter/src/pages/About.tsx
@@ -1,14 +1,38 @@
 import { Link } from "../components/Link";
 
-export function AboutPage() {
+const i18n = {
+  es: {
+    title: "Sobre mi",
+    subtitle: "Bienvenido a mi página de información",
+    description: "Soy Junior Lara, un programador apasionado por la tecnología.",
+    alt: "Imagen de perfil de Junior Lara con una camisa gris",
+    button: "Ir a la página principal"
+  },
+  en: {
+    title: "About Me",
+    subtitle: "Welcome to my information page",
+    description: "I am Junior Lara, a programmer passionate about technology.",
+    alt: "Profile picture of Junior Lara with a grey shirt",
+    button: "Go to Home Page"
+  }
+};
+
+const useI18n = (lang: string) => {
+  return i18n[lang as keyof typeof i18n] || i18n.en;
+};
+
+export function AboutPage({ routeParams } : { routeParams: Partial<Record<string, string | string[]>>} ) {
+  const i18n = useI18n(routeParams.lang as string ?? 'en');
   return (
     <div className="aboutpage">
-      <h1>About Page</h1>
+      <h1>{i18n.title}</h1>
+      <p>{i18n.subtitle}</p>
       <div>
-        <img src="https://avatars.githubusercontent.com/u/45317638?v=4" alt="Perfil picture of Junior Lara with a grey shirt" />
+        <img src="https://avatars.githubusercontent.com/u/45317638?v=4" alt={i18n.alt} />
       </div>
-      <p>Welcome to the About Page!</p>
-      <Link to="/">Go to Home Page</Link>
+      <span>{i18n.description}</span>
+
+      <Link to="/">{i18n.button}</Link>
     </div>
   )
 };
