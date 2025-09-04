94294cc0cf2779bc414033ba1be58560fad23f88
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-04 15:05:20 -0400

    Configuracion y prueba de tailwindcss

--- projects/07-CRUD_React_Redux/src/App.tsx
+++ projects/07-CRUD_React_Redux/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1>CRUD with React Redux</h1>
+    <h1 className="text-3xl font-bold text-center underline">CRUD with React Redux</h1>
   )
 }
 
--- projects/07-CRUD_React_Redux/tailwind.config.js
+++ projects/07-CRUD_React_Redux/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+    // path tremor node_modules
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
